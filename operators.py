b182198e0bcc188ca360bfaf7e5e29a2b9d0af39
Anirudh Kaushal <anirudhkushal30@gmail.com>
2023-08-09 22:25:38 -0400

    operators lesson completed

---
+++ operators.py
@@ -0,0 +1,11 @@
+meaning = 42
+
+print('')
+
+# if meaning > 10:
+#     print('right on!')
+# else:
+#     print('not today')
+
+# ternary operators
+print('right on!') if meaning > 10 else print('not today')
