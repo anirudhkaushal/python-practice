04aa2dc1295e06b095c3a866087e35034a6c32c5
Anirudh Kaushal <anirudhkushal30@gmail.com>
2023-08-09 22:03:42 -0400

    intro lesson completed

--- intro.py
+++ intro.py
@@ -1,1 +1,11 @@
-print("hello world")
+line01 = "********************" # header/footer
+line02 = "*                  *" # re-use
+line03 = "*     WELCOME!     *" # title
+
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
+
